f55942639fefee650bfe56f92366f7f39e97f24c
Bence Ferdinandy <bence@ferdinandy.com>
2024-11-18 16:09:26 +0100

    refs: add create_only option to refs_update_symref_extended
    Allow the caller to specify that it only wants to update the symref if
    it does not already exist. Silently ignore the error from the
    transaction API if the symref already exists.
    
    Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/remote.c
+++ builtin/remote.c
@@ -1476,7 +1476,7 @@ static int set_head(int argc, const char **argv, const char *prefix)
 		goto cleanup;
 	}
 	updateres = refs_update_symref_extended(refs, b_head.buf, b_remote_head.buf,
-			"remote set-head", &b_local_head);
+			"remote set-head", &b_local_head, 0);
 	if (updateres == -2) {
 		result |= error(_("Could not setup %s"), b_head.buf);
 		goto cleanup;
