47f486844f18f745d18a4dc40a4a40de8b0e954a
Bence Ferdinandy <bence@ferdinandy.com>
2024-11-18 16:09:28 +0100

    fetch set_head: handle mirrored bare repositories
    When adding a remote to bare repository with "git remote add --mirror",
    running fetch will fail to update HEAD to the remote's HEAD, since it
    does not know how to handle bare repositories. On the other hand HEAD
    already has content, since "git init --bare" has already set HEAD to
    whatever is the default branch set for the user. Unless this - by chance
    - is the same as the remote's HEAD, HEAD will be pointing to a bad
    symref. Teach set_head to handle bare repositories, by overwriting HEAD
    so it mirrors the remote's HEAD.
    
    Note, that in this case overriding the local HEAD reference is
    necessary, since HEAD will exist before fetch can be run, but this
    should not be an issue, since the whole purpose of --mirror is to be an
    exact mirror of the remote, so following any changes to HEAD makes
    sense.
    
    Also note, that although "git remote set-head" also fails when trying to
    update the remote's locally tracked HEAD in a mirrored bare repository,
    the usage of the command does not make much sense after this patch:
    fetch will update the remote HEAD correctly, and setting it manually to
    something else is antithetical to the concept of mirroring.
    
    Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -1607,7 +1607,7 @@ static const char *strip_refshead(const char *name){
 
 static int set_head(const struct ref *remote_refs)
 {
-	int result = 0, updateres;
+	int result = 0, updateres, is_bare;
 	struct strbuf b_head = STRBUF_INIT, b_remote_head = STRBUF_INIT,
 		b_local_head = STRBUF_INIT;
 	const char *remote = gtransport->remote->name;
@@ -1640,15 +1640,21 @@ static int set_head(const struct ref *remote_refs)
 
 	if (!head_name)
 		goto cleanup;
-	strbuf_addf(&b_head, "refs/remotes/%s/HEAD", remote);
-	strbuf_addf(&b_remote_head, "refs/remotes/%s/%s", remote, head_name);
+	is_bare = is_bare_repository();
+	if (is_bare) {
+		strbuf_addstr(&b_head, "HEAD");
+		strbuf_addf(&b_remote_head, "refs/heads/%s", head_name);
+	} else {
+		strbuf_addf(&b_head, "refs/remotes/%s/HEAD", remote);
+		strbuf_addf(&b_remote_head, "refs/remotes/%s/%s", remote, head_name);
+	}
 		/* make sure it's valid */
-	if (!refs_ref_exists(refs, b_remote_head.buf)) {
+	if (!is_bare && !refs_ref_exists(refs, b_remote_head.buf)) {
 		result = 1;
 		goto cleanup;
 	}
 	updateres = refs_update_symref_extended(refs, b_head.buf, b_remote_head.buf,
-					"fetch", &b_local_head, 1);
+					"fetch", &b_local_head, !is_bare);
 	if (updateres == -1) {
 		result = 1;
 		goto cleanup;
